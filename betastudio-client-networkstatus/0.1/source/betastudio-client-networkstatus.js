d126ec4f79ddcef8a99e91257009bff6c59654f3
Samir Dash <sdash@redhat.com>
2017-09-22 06:38:28 +0530

    Add files via upload

---
+++ betastudio-client-networkstatus/0.1/source/betastudio-client-networkstatus.js
@@ -0,0 +1,179 @@
+/*
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                .'                                                                                                                                                                   
+                               @@@@.                                                                                                                                                                 
+                             @@@@@@@@                                                                                                                                                                
+                           ;@@@+  ,@@@@                                                                                                                                                              
+                         `@@@@      '@@@;                                                                                                                                                            
+                        @@@@`         @@@@.                                                                                                                                                          
+                      +@@@:            `@@@@                                                                                                                                                         
+                    ,@@@#                ,@@@#                                                                                                                                                       
+                  `@@@@                    +@@@:                                                                                                                                                     
+                 @@@@.                       @@@@`                                                                                                                                                   
+               '@@@'                          `@@@@                                                                                                                                                  
+             .@@@@                              :@@@+                                                                                                                                                
+            @@@@`                                 #@@@,                                                                                                                                              
+           @@@:                                     @@@`                                                                                                                                             
+           @@@@`                                  `@@@@`                                                                                                                                             
+           @@@@@@                                @@@@@@`                                                                                                                                             
+           @@ :@@@#                            +@@@: @@`                                                                                                                                             
+           @@   +@@@;                        ,@@@#   @@.        @@@,                                                    :@:  :;                                    ::   :@                           
+           @@     @@@@,                    `@@@@     @@.        @@@`                            :@@                    ;;                                          ::                                
+           @@      `@@@@                  @@@@`      @@.        @@@                            .@@@                    @           :.                              :.                                
+           @@        ,@@@@              ;@@@;        @@.        @@@                            :@@@                    @           :`                              :`                                
+           @@          +@@@:          `@@@@          @@.       `@@@@@@@@@+         @@@@@@    :@@@@@@@@    ;@@@@@@@     @          @@@@@@  +`         @       '@@@@@@    .+      @@@@@,               
+           @@            @@@@.       @@@@`           @@.       `@@@@@@@@@@@`     #@@@@@@@@+  :@@@@@@@@   @@@@@@@@@@'   ##          #      #          @     +@      +    :;    #'     .@              
+           @@             `@@@@    #@@@,             @@.       .@@@@@@@@@@@@    #@@@@  @@@@` ;@@@@@@@@   @:     @@@@    @+         @      #          @    #:       +    ::   ':       `#             
+           @@               ,@@@@:@@@+               @@.       :@@@     #@@@#   @@@,    @@@@   @@@@             +@@@     @#        @      @          +   .#        #    :.   @         @             
+           @@                 '@@@@@                 @@.       :@@@      @@@@  #@@@     `@@@   @@@@             @@@@      #@       @      @          ;   @         @    ;`  ,;         ;.            
+           @@                   @@`                +@@@,       '@@@      #@@@  @@@@@@@@@@@@@   @@@#          :@@@@@@       :@      @      @         `:   @         @    +   @          .'            
+           @@                   @@               ,@@@@@,       #@@@      ;@@@  @@@@@@@@@@@@@   @@@+       @@@@@@@@@@        .@     @      @         .:  .+         @    #   @           #            
+           @@                   @@             `@@@@ @@,       @@@@      #@@@  @@@;            @@@;     +@@@@@: +@@@         .@    @      @         :.  ::         @    @   @          .;            
+           @@                   @@            @@@@,  @@,       @@@@      @@@@  @@@#            @@@;    .@@@#    #@@@          #:   @      @         +   .;         @    @   @          '.            
+           @@                   @@          '@@@+    @@,       @@@@     `@@@+  @@@@            @@@;    @@@@     @@@@           @   @      @         @    @         @    @   ::         @             
+           @@                   @@        .@@@@      @@,       @@@@@`  `@@@@   `@@@@      :    @@@@    @@@@     @@@@          .+   @      @         #    @        #@    @    @         @             
+           @@                   @@       @@@@`       @@,       @@@;@@@@@@@@;    #@@@@@@@@@@    @@@@@@@ :@@@+  : @@@@          @    @      ;.       @@    .#      `;@    @    ''       @              
+           @@                   @@     #@@@:         @@,       @@@::@@@@@@'      +@@@@@@@@'    #@@@@@#  @@@@@@' @@@@  @;     @,    @:      #'    :@ @     ,@    '+ @    @     '@    .@               
+           @@                   @@   :@@@#           @@,             :@@:          `;@@@:        #@@:     ;@@:          :@@@:       :@@`     :@@:           :@@:                :@@;                 
+           @@                   @@ `@@@@             @@:                                                                                                                                             
+           @@                   @@@@@@.              @@:                                                                                                                                             
+           @@                   @@@@'                @@:                                                                                                                                             
+           @@@+                 @@@                ;@@@:                                                                                                                                             
+            #@@@:               @@               `@@@@                                                                                                                                               
+              @@@@`             @@              @@@@.                                                                                                                                                
+               `@@@@            @@            +@@@'                                                                                                                                                  
+                 :@@@+          @@          ,@@@@                                                                                                                                                    
+                   #@@@,        @@         @@@@`                                                                                                                                                     
+                     @@@@`      @@`      @@@@,                                                                                                                                                       
+                      .@@@@     @@`    ;@@@+                                                                                                                                                         
+                        ;@@@+   @@`  `@@@@                                                                                                                                                           
+                          #@@@: @@` @@@@`                                                                                                                                                            
+                            @@@@@@#@@@:                                                                                                                                                              
+                             `@@@@@@#                                                                                                                                                                
+                               :@@@                                                                                                                                                                  
+                                 .                                                                                                                                                                   
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+                                                                                                                                                                                                     
+*/
+
+var betaStudio_module_client_networkstatus_config = betaStudio_module_client.config;/* requires that already jquery, betastudio-client.js and beatstudio-client-authentication.js  are loaded */
+var betaStudio_module_client_networkstatus= function(config){
+/*********************************betaStudioModule Info*********************************/
+this.module_name = "betaStudio-Client-NetworkStaus";
+this.module_description = "This module handles the client side networkstatus in the betaStudio client."
+this.module_license_type = "MIT";
+this.module_dependancies = "jQuery|betaStudio_module_client.js|beatstudio-client-authentication.js|";/* if any dependancies like jquery etc, metion here as an info */
+this.module_version = "0.1.0";
+this.module_lastupdated = "22-09-2017";
+this.module_authors = "Samir Dash|";/* Additional authors append your name with a pipe (|) as suffix */
+this.module_author_emails = "sdash@redhat.com|";
+this.module_help = "|"/* Additional public methods and props append with a pipe (|) as suffix */
+/************************************Config**********************************/    
+if(!config){
+    
+    var config = {
+        
+    doc:         document,/* target document (with a body tag )object to apply */
+    testMode:    false, /* valid values are true|false */
+    
+   
+}; 
+if(_testMode){
+   console.log("ERROR: "+module_name+": Config not found. Make sure that jquery, betastudio-client.js and beatstudio-client-authentication.js  are loaded."); 
+}
+
+}else{
+    
+    var config = config;
+    
+}
+/************************************Public Properties*********************************/
+    this.config = config; 
+/************************************Public Methods************************************/
+ 
+ this.Logout = function(){
+     
+     _private_Logout();
+ }
+ this.Login = function(){
+     _private_ShowLogin();
+     
+ } 
+/*******************************Private Properties**************************************/
+    
+ 
+ 
+    
+     
+    
+/*******************************Private Methods**************************************/
+ 
+var _private_initOffline_banner = function (_doc){
+    if(!_doc){var _doc = document; } 
+    var str =  '<div id="snoop_offline_indicator" style="z-index:9999999999999;"><div class="offline_wrap"><h1>No network!</h1><p>Could not connect to server!</p><image src="betaStudiologo_square.png"  ><h3>Enable the network to use betaStudio!</h3></div></div>';
+    var banner = _doc.createElement("SPAN");
+    banner.id="snoop_offline_indicator_wrap";
+    banner.innerHTML=str;
+    _doc.body.appendChild(banner);
+    _private_updateIndicator (_doc);
+}
+var _private_updateIndicatorOff = function (event){
+    
+    if(!_doc){var _doc = config.doc; }
+    _doc.getElementById(betaStudio_module_client.config.betaStudioClientOfflineIndicator).style.display= "block"; 
+    betaStudio_module_client_authentication.Check_Auth();  //also authenticate if the user is logged in or not
+}
+var _private_updateIndicatorOn = function (event){
+    
+    if(!_doc){var _doc = config.doc; }
+    if(window.location.href.indexOf(betaStudio_module_client.config.betaStudioClientWelcomeURL)> -1 && document.title.indexOf("Loading...") > -1){
+    window.location  = betaStudio_module_client.config.betaStudioClientURL;
+    _doc.getElementById(betaStudio_module_client.config.betaStudioClientOfflineIndicator).style.display= "none"; 
+    }else{
+    _doc.getElementById(betaStudio_module_client.config.betaStudioClientOfflineIndicator).style.display= "none"; 
+    }
+}
+var _private_updateIndicator = function (_doc){
+    if(!_doc){var _doc = config.doc; }
+    if (navigator.onLine) {
+    _private_updateIndicatorOn(_doc);
+    } else {
+    _private_updateIndicatorOff(_doc);
+    }
+}
+var _private_Init = function(_doc, _testMode){
+    if(!_doc){var _doc = document; }
+    if(_testMode){
+        console.log("***********************************************************************************************************************************");
+        console.log("betaStudio::"+module_name+" [Initiated with testMode as true.]");
+        console.log("module_version:"+module_version+"");
+        console.log("module_lastupdated:"+module_lastupdated+"");
+        console.log("module_license_type:"+module_license_type+"");
+        console.log("module_description:"+module_description+"");
+        console.log("module_authors:"+module_authors+"");
+        console.log("module_author_emails:"+module_author_emails+"");
+        console.log("module_help:"+module_help+"");
+        console.log("***********************************************************************************************************************************");
+ 
+    }
+    _private_initOffline_banner();
+    // Update the online status icon based on connectivity
+    window.addEventListener('online',  _private_updateIndicatorOn);
+    window.addEventListener('offline', _private_updateIndicatorOff);
+
+ 
+}
+_private_Init(config.doc, config.testMode);
+return this;
+}(betaStudio_module_client_networkstatus_config);
